f19b3d73ec3b795176f0a2167da4cb382d9e3116
Gabriel C. Ullmann <ullmanng162@gmail.com>
2025-10-31 09:18:22 -0400

    improve config error reporting

--- src/config.py
+++ src/config.py
@@ -19,3 +19,7 @@ KAFKA_AUTO_OFFSET_RESET = os.getenv("KAFKA_AUTO_OFFSET_RESET")
 # Application Configuration
 OUTPUT_DIR = os.getenv("OUTPUT_DIR")
 LOG_LEVEL = os.getenv("LOG_LEVEL")
+
+for env_variable in ["KAFKA_HOST", "KAFKA_TOPIC", "KAFKA_GROUP_ID", "KAFKA_AUTO_OFFSET_RESET", "OUTPUT_DIR", "LOG_LEVEL"]:
+    if globals()[env_variable] is None:
+        raise EnvironmentError(f"Variable {env_variable} n'était pas trouvé dans votre fichier .env.")
